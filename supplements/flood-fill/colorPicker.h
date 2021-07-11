068d52631aec7e1d356d74fac5f8f13b89fbc0c5
NikhilMJagtap <nikhil.jagtap@spit.ac.in>
2021-07-11 13:23:28 +0530

    added dependecies of filler and colour pickers

---
+++ supplements/flood-fill/colorPicker.h
@@ -0,0 +1,49 @@
+/**
+ * @file colorPicker.h
+ *
+ * Definition of a color picker abstract base class.
+ *
+ */
+#ifndef _COLORPICKER_H_
+#define _COLORPICKER_H_
+
+#include "cs221util/HSLAPixel.h"
+#include "cs221util/PNG.h"
+
+#include "point.h"
+
+using namespace cs221util;
+
+/**
+ * colorPicker: a functor that determines the color that should be used
+ * given an x and y coordinate.
+ *
+ */
+class ColorPicker
+{
+  public:
+    /**
+     * Destructor: does nothing, but as it is virtual, you may overload it in
+     * derived classes if needed.
+     */
+    virtual ~ColorPicker(){};
+
+    /**
+     * Picks the color for pixel (x, y).
+     *
+     * This function **must** be defined in the subclass! It chooses a color for
+     * the given point and returns it.
+     *
+     * For example, the NegativeColorPicker::operator()() function is defined so as
+     * to return the negative color of the pixel at the specified location of the original image.
+     *
+     * For the other patterns, you will have to figure out how to "pick" the
+     * colors.
+     *
+     * @param p   The point for which you're picking a color.
+     * @return    The color chosen for (p).
+     */
+    virtual HSLAPixel operator()(point p) = 0;
+};
+
+#endif
